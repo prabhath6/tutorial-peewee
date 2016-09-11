b0989b0ca066ee587b9749637f240d300ee81f23
prabhath6 <prabhathkiran20@gmail.com>
2016-09-11 14:55:52 -0700

    add one to one

---
+++ core/one_to_one.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from peewee import *
+
+psql_db = PostgresqlDatabase('orm1', user='prabhath')
+
+
+class BaseModel(Model):
+    """A base model that will use our Postgresql database"""
+    class Meta:
+        database = psql_db
+
+
+class Voter(BaseModel):
+    id = PrimaryKeyField(null=False)
+    name = CharField(max_length=100)
+    age = SmallIntegerField()
+
+
+class Vote(BaseModel):
+    user = ForeignKeyField(Voter, primary_key=True)
+    candidate_name = CharField(max_length=50)
+
+
+if __name__ == "__main__":
+
+    psql_db.create_tables([Voter, Vote], safe=True)
+
+    # create voter
+    vot, status = Voter.create_or_get(name="Sam", age=25)
+
+    # vote
+    vote1 = Vote.create(user=vot, candidate_name="Hilary")
+    #  vote2 = Vote.create(user=vot, candidate_name="Trump")
+
+    """
+    Second statement will result in IntegrityError: duplicate key value violates unique constraint "vote_pkey"
+    as one user can cast oonly one vote.
+    """
+
+    print("done")
